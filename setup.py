ae3849471bd10aa5e3616067a32ff2010354bd4c
Julia Evans <julia@stripe.com>
2015-01-07 20:42:57 -0500

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='topmodel',
-      version='0.1.1',
+      version='0.1.2',
       description='Model evaluation',
       author='Julia Evans',
       author_email='julia@stripe.com',
